822ccee43e58adee492f9a99361744a4456b3cc1
tdcline1 <tdcline1@aol.com>
2025-01-07 11:11:36 -0500

    done with step 3.8

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,6 +4,7 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalQuantity: 0, // New property to store the total quantity of items
   },
   reducers: {
     addItem: (state, action) => {
@@ -11,18 +12,26 @@ export const CartSlice = createSlice({
         const existingItem = state.items.find(item => item.name === name);
         if (existingItem) {
             existingItem.quantity++;
+            state.totalQuantity++;
         } else {
             state.items.push({ name, image, cost, quantity: 1 });
+            state.totalQuantity++; // Increment total quantity on new item
         }
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+        const itemToRemove = state.items.find(item => item.name === action.payload);
+        
+        if (itemToRemove) {
+            state.totalQuantity -= itemToRemove.quantity; // Update total quantity when an item is removed
+            state.items = state.items.filter(item => item.name !== action.payload);
+        }
     },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
         const itemToUpdate = state.items.find(item => item.name === name);
         if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
+            state.totalQuantity += quantity - itemToUpdate.quantity;
+            itemToUpdate.quantity = quantity;
         }
     },
   },
